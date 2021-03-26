084c4d954e877b77632480b4642da71ed5fb0405
nirmala0508 <nirmalaik14murugar@gmail.com>
2021-03-26 16:46:21 +0530

    second push

--- src/org/test/Data.java
+++ src/org/test/Data.java
@@ -5,6 +5,10 @@ public class Data {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello");
+		System.out.println("World");
+		int a=10;
+		System.out.println(a);
+		
 		
 	}
 	}
